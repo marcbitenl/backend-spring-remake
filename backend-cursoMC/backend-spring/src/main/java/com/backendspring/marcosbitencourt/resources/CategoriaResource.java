ea9e8f7a9e86ab9ce35453d03817c2fb88ba4810
marcbitenl <marcbitenl@gmail.com>
2020-06-17 23:08:18 -0300

    Deletando uma Categoria com DELETE

--- backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/CategoriaResource.java
+++ backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ package com.backendspring.marcosbitencourt.resources;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,7 +44,13 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 		
-	
-	
-	
+	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+			service.delete(id);
+			return ResponseEntity.noContent().build();
+			}
 }
+	
+	
+	
+
--- backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/services/CategoriaService.java
+++ backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.backendspring.marcosbitencourt.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.backendspring.marcosbitencourt.domain.Categoria;
 import com.backendspring.marcosbitencourt.repositories.CategoriaRepository;
+import com.backendspring.marcosbitencourt.services.exceptions.DataIntegrityException;
 import com.backendspring.marcosbitencourt.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,17 @@ public class CategoriaService {
 		find(obj.getId()); 
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+			}
+			catch(DataIntegrityViolationException e) {
+				throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+				
+			}
+
+	}
+	
 }
