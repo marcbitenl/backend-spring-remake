ff9c7a26684c9414cfefdeeb85e1d522d8c105bd
marcbitenl <marcbitenl@gmail.com>
2020-06-18 17:39:32 -0300

    Inserindo um novo Cliente com POST

--- backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/ClienteResource.java
+++ backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/ClienteResource.java
@@ -1,6 +1,7 @@
 package com.backendspring.marcosbitencourt.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.backendspring.marcosbitencourt.domain.Cliente;
 import com.backendspring.marcosbitencourt.dto.ClienteDTO;
+import com.backendspring.marcosbitencourt.dto.ClienteNewDTO;
 import com.backendspring.marcosbitencourt.services.ClienteService;
 
 @RestController
@@ -31,6 +34,15 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 	Cliente obj = service.find(id);
 	return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> Insert(@Valid @RequestBody ClienteNewDTO objDTO) {
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
