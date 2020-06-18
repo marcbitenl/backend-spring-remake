7b87d3f6c18920c55237da73a35ed06c5cfe3b88
marcbitenl <marcbitenl@gmail.com>
2020-06-17 23:23:13 -0300

    Listando todas as categorias com DTO

--- backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/CategoriaResource.java
+++ backend-cursoMC/backend-spring/src/main/java/com/backendspring/marcosbitencourt/resources/CategoriaResource.java
@@ -2,9 +2,10 @@ package com.backendspring.marcosbitencourt.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.backendspring.marcosbitencourt.domain.Categoria;
+import com.backendspring.marcosbitencourt.dto.CategoriaDTO;
 import com.backendspring.marcosbitencourt.services.CategoriaService;
 
 @RestController
@@ -48,7 +50,14 @@ public class CategoriaResource {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 			service.delete(id);
 			return ResponseEntity.noContent().build();
-			}
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity <List<CategoriaDTO>> findAll() {
+	List<Categoria> list = service.findAll();
+	List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+	return ResponseEntity.ok().body(listDTO);
+	}
 }
 	
 	
